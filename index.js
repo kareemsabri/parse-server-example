710278c3e1d0e6c93f8f2b338b63eca7dad83d9b
Kareem Sabri <kareem.sabri@gmail.com>
2016-08-04 19:50:57 -0400

    more config

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   clientKey: process.env.CLIENT_KEY || '',
+  facebookAppIds: [process.env.FACEBOOK_APP_ID],
+  fileKey: process.env.FILE_KEY,
   liveQuery: {
     classNames: ["Matches", "Messages"] // List of classes to support for query subscriptions
   }
